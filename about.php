ea93ed754b60cdf588b97afc297beac064e3d6b4
Nusry Nizam <nusrynizam@gmail.com>
2021-11-21 12:28:03 +0530

    added about page

---
+++ about.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <!-- <link rel="stylesheet" href="styles-login.css"> -->
+    <title>ePocket | About Us</title>
+</head>
+
+<body>
+    <!-- Navigation Bar -->
+    <?php include 'hf/nav.php' ?>
+
+    <!-- Main content -->
+    <section id="section1">
+        <div id="section1-left">
+            <h1 class="h1-light">About Us.</h1>
+            <p class="p-light">
+                We are a group of IT undergraduates currently working on this project. 
+                The development of this project is ongoing and we are currently in the process of adding new features.
+            </h6>
+                <br><br>
+                <p class="p-light members">
+                    <strong>Members:</strong><br>
+                    <a href="https://github.com/nusrynizam" target="_blank" rel="noreferrer">Nusry</a>
+                    <br>
+                    <a href="https://github.com/akeesham" target="_blank" rel="noreferrer">Akeel</a>
+                    <br>
+                    <a href="https://github.com/" target="_blank" rel="noreferrer">Zaroath</a>
+                </p>
+                <!-- <button class="btn-light get-started">Get Started</button> -->
+        </div>
+        <div id="section1-right">
+            <img src="assets/about.svg" alt="About-us" style="width: 40vw">
+        </div>
+    </section>
+
+    <?php include 'hf/footer.php' ?>
+
+</body>
+<script src="script.js"></script>
+
+</html>
